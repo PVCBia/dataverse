5e272e3e75df3a1389fee9824186389de5fda5e4
PVCBia <pvcbeatriz@gmail.com>
2024-03-04 23:08:28 -0300

    ajuste dos dados a serem exibidos

--- src/view.js
+++ src/view.js
@@ -1,18 +1,23 @@
 export const renderItems = (data) => {
 
-  // const cards = document.createElement('ul');
-  // cards.classList.add('cartaoUnitario');
-
-  let result = '<ul>'
+    let result = '<ul>'
   
   // colocar "itemtype" na li
+  // inserir "itemprop" ???
 
   data.forEach((element) => {
     result += `
     <li class = "cardsUnitarios">
-    <p>${element.name}</p>
     <img src= ${element.imageUrl} alt= "">
-    <p>${element.description}</p> 
+    <strong><p>${element.name}</strong></p>
+    <strong><p>${element.shortDescription}</p></strong>
+    <strong><p>Sobre:</strong> ${element.description}</p>
+    <strong><p>Idade:</strong> ${element.facts.idade}</p>
+    <strong><p>Nacionalidade:</strong> ${element.facts.nacionalidade}</p>
+    <strong><p>Área de atuação:</strong> ${element.facts.areaDeAtuacao}</p>
+    <strong><p>Trabalho mais relevante:</strong> ${element.facts.trabalhoMaisRelevante}</p>
+    <strong><p>Escândalo:</strong> ${element.facts.escandalo}</p>
+    <strong><p>Curiosidade:</strong> ${element.facts.curiosidade}</p>
     </li>
     `;
   })
