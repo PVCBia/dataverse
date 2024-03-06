a5c1b163acc9c90efe5d348ab7a94983ae028446
PVCBia <pvcbeatriz@gmail.com>
2024-03-06 17:24:51 -0300

    inserção do teste para computeStats

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -2,9 +2,8 @@
 //elementos de cada item e deve retornar um elemento DOM ou uma string de HTML.
 
 import { filterData, sortData, computeStats } from '../src/dataFunctions.js';
-import { data as fakeData } from './data.js';
+import { data as fakeData, mockAscendente, mockDescendente} from './data.js';
 
-//console.log(fakeData);
 
 describe ('testes da função filterData', () => {
 
@@ -20,19 +19,31 @@ describe ('testes da função filterData', () => {
 });
 
 describe('testes da função sortData', () => {
+  escolherOrdem
 
   it('deve retornar a lista em ordem crescente', () => {
-    expect(sortData()).toBe('sortData');
+    const dadosOrdenados = sortData(fakeData, 'name', 'asc')
+    expect(dadosOrdenados()).toStrictEqual(mockAscendente);
   });
 
   it('deve retornar a lista em ordem decrescente', () => {
-    expect(sortData()).toBe('sortData');
+    const dadosOrdenados = sortData(fakeData, 'name', 'desc')
+    expect(dadosOrdenados()).toStrictEqual(mockDescendente);
   });
 });
 
 describe('testes da função computeStats', () => {
 
-  it('deve retornar a estatística da média de idade das celebridades', () => {
-    expect(computeStats()).toBe('computeStats');
+  it('deve retornar a quantidade de celebridades da lista', () => {
+    const stats = [...fakeData].map(item => item.facts).filter(value => !isNaN(value));
+    //cria um novo array stats que contém apenas os valores facts
+    //do array fakeData que podem ser convertidos em números
+    const count = stats.reduce((accumulator) => accumulator + 1, 0);
+    //calcula a quantidade de itens no array stats usando o
+    //método reduce.
+    expect(count).toBe(computeStats(fakeData));
+    //verifica se a variável count é igual ao valor retornado
+    //pela função computeStats quando o array fakeData é 
+    //passado como argumento
+  })
   });
-});
