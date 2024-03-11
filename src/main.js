8b54e3da9fc23635d62d296f348ab9bfc001ab6e
PVCBia <pvcbeatriz@gmail.com>
2024-03-11 00:15:53 -0300

    limpeza do código para melhor visualização

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { filterData, sortData } from './dataFunctions.js';
-// computeStats
+import { filterData, sortData, computeStats } from './dataFunctions.js';
+
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
-
-//console.log(example, renderItems(data), data);
 
 //      renderiza os cards pelo root/html
 const listaCards = document.querySelector('#root');
@@ -22,6 +20,7 @@ filtroNacionalidade.addEventListener('change', (event) => {
   const valorDoFiltro = event.target.value;
   const cardsFiltrados = filterData(data, 'nacionalidade', valorDoFiltro);
   dadosRenderizados = cardsFiltrados; // Atualiza os dados renderizados
+  //celebridadesMostradas(dadosRenderizados); *****
   mostraCards(dadosRenderizados);
 });
 
@@ -33,21 +32,32 @@ filtroOrdem.addEventListener('change', () => {
   mostraCards(dadosRenderizados);
 });
 
+//      estatística - número total de cards renderizados
+const celebridadeSpan = document.querySelector(".quantidadeCelebridades");
+document.addEventListener("DOMContentLoaded", () => {
+  celebridadesMostradas(data);
+})
+
+//      atualiza o número de celebridades mostradas
+const celebridadesMostradas = (data) => {
+  const numeroCelebridades = computeStats(data);
+  celebridadeSpan.innerText = `${numeroCelebridades} celebridades.`;
+}
+
+
+
 //      botão de limpar / definindo valor índece zero
 const limparSelecoes = document.getElementById('botaoLimpar');
 limparSelecoes.addEventListener('click', () => {
-console.log(data)
   // // Define o valor selecionado como a primeira opção (índice 0)/ volta a configuração "inicial"
   filtroNacionalidade.selectedIndex = 0;
   filtroOrdem.selectedIndex = 0;
 
+  // celebridadesMostradas (data);
   
-  // disparar eventos na implementação do event model
-  // chama manualmente o evento 'change' para atualizar a lista de cartões
-  // filtroNacionalidade.dispatchEvent(new Event('change'));
-  // filtroOrdem.dispatchEvent(new Event('change'));
-
-  //rever e corrigir essa segunda parte, de acordo com o filtro ordem
+  //      reinicia o span quantidade de celebridade
+  //celebridadeSpan = celebridadesMostradas;
+  
   //limpa e renderiza todos os cards, porém mantem a última configuração
   dadosRenderizados = data; // Reinicia os dados renderizados
   //listaCards.innerHTML = renderItems(data)
@@ -56,49 +66,7 @@ console.log(data)
 
 
 
-
-
-
-//      filtro ordem - depois do filtro nacionalidade pronto
-// const filtroOrdem = document.getElementById('filtroOrdem');
-// filtroOrdem.addEventListener('change', (event) => {
-//   const valorDoFiltro = event.target.value;
-//   const cardsOrdenados = sortData(data, 'name', valorDoFiltro);
-//   mostraCards(cardsOrdenados);
-// });
-
-
-// const filtroOrdem = document.getElementById('filtroOrdem');
-// filtroOrdem.addEventListener('change', () => {
-//   const valorDoFiltro = filtroNacionalidade.value;
-//   const cardsFiltrados = filterData(data, 'nacionalidade', valorDoFiltro);
-//   const valorDaOrdem = filtroOrdem.value;
-//   const cardsOrdenados = sortData(cardsFiltrados, 'name', valorDaOrdem);
-//   mostraCards(cardsOrdenados);
-// });
-
-
-
-
-
-
-//      segundo botão de limpar
-// const limparSelecoes = document.getElementById('botaoLimpar');
-// limparSelecoes.addEventListener('click', () => {
-//   document.getElementById("todos").selected = "true";
-//   //
-// });
-
-
-//      primeiro botão de limpar - zera o valor de todo select
-// const limparSeleções = document.getElementById('botaoLimpar');
-// limparSeleções.addEventListener('click', () => {
-//     const valorDoFiltro = event.target.value;
-//     listaCards.innerHTML = "";
-//     filtroOrdem.innerHTML = "";
-//     filtroNacionalidade.innerHTML = "brasileira";
-//     listaCards.innerHTML = renderItems(data)
-// });
+//      implementações futuras:
 
 
 //          modificar para pop-up
